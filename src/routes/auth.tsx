16ef1f972b38dbfdf8194c4dec9f1977b1249719
Hoai Tino <hoaicoder0503@gmail.com>
2025-08-14 14:28:20 +0700

    feat: create role access and authorized

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -2,18 +2,23 @@ import type { RouteObject } from "react-router";
 import DashboardLayout from "../app/layouts/DashboardLayout";
 import Dashboard from "../features/dashboard/dashboard";
 import UserManagement from "../features/user-management/user";
+import ProtectedRoute from "../components/ProtectedRoute";
+
+export function withRole(element: React.ReactNode, roles: string[]) {
+  return <ProtectedRoute allowedRoles={roles}>{element}</ProtectedRoute>;
+}
 
 export const AuthRoute: RouteObject = {
   path: "admin",
-  element: <DashboardLayout />,
+  element: withRole(<DashboardLayout />, ["ADMIN", "EMPLOYEE"]),
   children: [
     {
       index: true,
-      element: <Dashboard />,
+      element: withRole(<Dashboard />, ["ADMIN", "EMPLOYEE"]),
     },
     {
       path: "users-management",
-      element: <UserManagement />,
+      element: withRole(<UserManagement />, ["ADMIN"]),
     },
   ],
 };
